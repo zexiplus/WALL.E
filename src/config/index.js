47a9c6e55d933e025fa5c82aa0e2aae829b8850a
zexiplus <zexiplus@outlook.com>
2017-11-24 17:57:37 +0800

    led错误修复

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const cameraUrl = 'http://192.168.17.149:8080/?action=stream'
-const raspiIp = 'http://192.168.17.149:3000'
+const cameraUrl = 'http://192.168.41.89:8080/?action=stream'
+const raspiIp = 'http://192.168.41.89:3000'
 export {cameraUrl,raspiIp}
