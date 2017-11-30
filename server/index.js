aec58ffd34c4f7cdb5fe573ac81571036c0d139f
zexiplus <zexiplus@outlook.com>
2017-11-30 18:04:44 +0800

    rewrite serverSide

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var express = require('express')
+var cors = require('cors')
 
 //turn on camera
 cp.exec("/home/pi/software/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -i '/home/pi/software/mjpg-streamer/mjpg-streamer-experimental/input_uvc.so -d /dev/video0 -n -y -f 25 -r 640x480' -o '/home/pi/software/mjpg-streamer/mjpg-streamer-experimental/output_http.so -n -w /usr/local/www'") 
@@ -18,13 +19,14 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extend: false}))
 app.use(cookieParser())
+app.use(cors())
 app.post('/board/led',function(req,res,next) {
     console.log('req is ',req)
     res.end(JSON.stringify({
       hello: 'world'
     }))
 })
-
+server.listen(3030)
 
 
 var board = new five.Board({
