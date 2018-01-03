046def7a64320633db5e360b55cf74e66dda1b8e
zexiplus <zexiplus@outlook.com>
2018-01-03 20:24:16 +0800

    write intergration page

--- src/components/Camera/index.vue
+++ src/components/Camera/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="cameraContainer">
         <el-row :gutter="50">
-            <el-col :span="16">
-                <el-card>
-                    <h1 slot="header" class="textAlign">
+            <el-col :span="fullCamera ? 14 : 24">
+                <el-card :body-style="fullCamera ? {} : cardStyle">
+                    <h1 v-if="fullCamera" slot="header" class="textAlign">
                         摄像机
                     </h1>
                     <el-row>
@@ -11,9 +11,14 @@
                             <iframe :src="urlType + url" id="camera"></iframe>
                         </el-col>
                     </el-row>
+                    <el-row v-if="!fullCamera">
+                        <el-col :offset="1" :span="22">
+                            <el-slider v-model="rotateAngle" :min="0" :max="180"></el-slider>
+                        </el-col>
+                    </el-row>
                 </el-card>
             </el-col>
-            <el-col :span="6">
+            <el-col :span="6" v-if="fullCamera">
                 <el-card>
                     <h1 slot="header" class="textAlign">
                         旋转相机
@@ -32,11 +37,15 @@
     export default {
         name: 'camera',
         props: {
-
+            fullCamera: {
+                type: Boolean,
+                default: true
+            }
         },
         data() {
             return {
-                rotateAngle: 90
+                rotateAngle: 90,
+                cardStyle: {padding: "0px",paddingTop: '10px'}
             }
         },
         computed: {
@@ -68,9 +77,11 @@
 </script>
 <style scoped>
     #camera {
-        width: 640px;
-        height: 480px;
+        width: 600px;
+        height: 400px;
         border: 0;
+        padding: 0;
+        transform: rotate(180deg);
     }
     .cameraAlign {
         text-align: center;
--- src/components/Control/index.vue
+++ src/components/Control/index.vue
@@ -1,11 +1,11 @@
 <template>
    <div class="controlContainer">
         <el-row :gutter="50">
-            <el-col :span="16">
+            <el-col :span=" fullControl ? 16 : 24">
                 <el-card>
-                    <h1 slot="header" class="textAlign">
+                    <h1 v-if="fullControl" slot="header" class="textAlign">
                         方向遥控
-                    </h1>                    
+                    </h1>
                     <el-form>
                     <el-row>
                         <el-col :span="21" class="canvasAlign">
@@ -20,13 +20,13 @@
                                 <el-slider v-model="speedRight" :min="0" :max="100"></el-slider>
                             </el-form-item>
                         </el-col>
-                    </el-row>    
+                    </el-row>
                     <el-row>
                         <el-col :span="4" :offset="2" class="canvasAlign">
                             <el-form-item label="遥控" label-width="50px">
                                 <el-switch v-model="remoteMode"></el-switch>
                             </el-form-item>
-                        </el-col>                        
+                        </el-col>
                         <el-col :span="4" class="canvasAlign">
                             <el-form-item label="避障" label-width="50px">
                                 <el-switch v-model="avoidMode"></el-switch>
@@ -36,19 +36,19 @@
                             <el-form-item label="循迹" label-width="50px">
                                 <el-switch v-model="trailMode"></el-switch>
                             </el-form-item>
-                        </el-col>   
+                        </el-col>
                         <el-col :span="4" class="canvasAlign">
                             <el-form-item label="旋转" label-width="50px">
                                 <el-switch v-model="rotateMode"></el-switch>
                             </el-form-item>
-                        </el-col> 
+                        </el-col>
                         <el-col :span="4" class="canvasAlign">
                             <el-form-item label="倒退" label-width="50px">
                                 <el-switch v-model="backMode"></el-switch>
                             </el-form-item>
-                        </el-col>                                                                   
-                    </el-row>      
-                    <el-row>
+                        </el-col>
+                    </el-row>
+                    <el-row v-if="fullControl">
                         <el-col :span="10" :offset="0">
                             <div class="box" >
                                 <el-row>
@@ -65,15 +65,15 @@
                                     </el-col>
                                     <el-col :span="4" :offset="1">
                                         <i ref="right" class="el-icon-caret-right"></i>
-                                    </el-col>                                    
+                                    </el-col>
                                 </el-row>
                             </div>
                         </el-col>
-                    </el-row>                                   
+                    </el-row>
                     </el-form>
-                </el-card> 
+                </el-card>
             </el-col>
-            <el-col :span="6">
+            <el-col :span="6" v-if="fullControl">
                 <el-card>
                     <h1 slot="header" class="textAlign">
                         当前方向
@@ -83,14 +83,20 @@
                             <div class="currDirection">{{currDirection}}</div>
                         </el-col>
                     </el-row>
-                </el-card>                
+                </el-card>
             </el-col>
         </el-row>
-    </div>    
+    </div>
 </template>
 <script>
     export default {
         name: 'thermometer',
+        props: {
+            fullControl: {
+                type: Boolean,
+                default: true
+            }
+        },
         data() {
             return {
                 speedLeft: 100,
@@ -116,7 +122,7 @@
             avoidMode(val) {
                 if(val) {
                     this.remoteMode = this.trailMode = this.rotateMode = this.backMode = false
-                }                
+                }
             },
             trailMode(val) {
                 if(val) {
@@ -131,7 +137,7 @@
             backMode(val) {
                 if(val) {
                     this.avoidMode = this.trailMode = this.rotateMode = this.remoteMode = false
-                }                
+                }
             }
         },
         created() {
@@ -140,7 +146,7 @@
         methods: {
             bindControl() {
                 document.addEventListener('keydown',this.captureKeyPress)
-                document.addEventListener('keyup',this.captureKeyUp)                
+                document.addEventListener('keyup',this.captureKeyUp)
             },
             //解绑键盘事件
             removeControl() {
@@ -156,44 +162,61 @@
                 this.currDirection = ''
             },
             captureKeyPress(e) {
+                console.log('emit')
                 switch(e.keyCode) {
-                    case 37: 
-                    this.setHeighLight('left')
+                    case 37:
+                    if(this.fullControl) {
+                        this.setHeighLight('left')
+                    }
                     this.driveMoto('left')
                     break;
                     case 38:
-                    this.setHeighLight('forward')
+                    if(this.fullControl) {
+                        this.setHeighLight('forward')
+                    }
                     this.driveMoto('forward')
                     break;
                     case 39:
-                    this.setHeighLight('right')
+                    if(this.fullControl) {
+                        this.setHeighLight('right')
+                    }
                     this.driveMoto('right')
                     break;
                     case 40:
-                    this.setHeighLight('back')
+                    if(this.fullControl) {
+                        this.setHeighLight('back')
+                    }
                     this.driveMoto('back')
                     break;
                 }
             },
             captureKeyUp(e) {
                 switch(e.keyCode) {
-                    case 37: 
-                    this.removeHeithLight('left')
+                    case 37:
+                    if(this.fullControl) {
+                        this.removeHeithLight('left')
+                    }
                     this.stopMoto('left')
                     break;
                     case 38:
-                    this.removeHeithLight('forward')
+                    if(this.fullControl) {
+                        this.removeHeithLight('forward')
+                    }
                     this.stopMoto('forward')
                     break;
                     case 39:
-                    this.removeHeithLight('right')
+                    if(this.fullControl) {
+                        this.removeHeithLight('right')
+                    }
                     this.stopMoto('right')
                     break;
                     case 40:
-                    this.removeHeithLight('back')
+                    if(this.fullControl) {
+                        this.removeHeithLight('back')
+                    }
                     this.stopMoto('back')
                     break;
-                }                
+                }
             },
             setHeighLight(refName) {
                 this.$refs[refName].style.color = '#409EFF'
@@ -201,7 +224,7 @@
             removeHeithLight(refName) {
                 this.$refs[refName].style.color = '#CFD3DB'
             }
-        }        
+        }
     }
 </script>
 <style scoped>
