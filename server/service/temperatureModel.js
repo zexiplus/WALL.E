88f163bf1e34fe3b8801380716d83c9862cc5bd6
zexiplus <zexiplus@outlook.com>
2018-01-11 19:01:13 +0800

    查询温度记录

--- server/service/temperatureModel.js
+++ server/service/temperatureModel.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose')
 var temSchema = mongoose.Schema({
     time: {
-	type: String,
-	required: true
+      	type: String,
+      	required: true
     },
     temValue: {
-	type: Number,
-	required: true
+      	type: Number,
+      	required: true
     }
 })
-    
+
 temSchema.methods.saveAndLog = function() {
     return this.save((err,doc) => {
-	if(err) console.log('error at saving temperature')
+	     if(err) console.log('error at saving temperature')
     })
 }
 var Temperature = mongoose.model('temperature',temSchema)
-module.exports = Temperature 
+module.exports = Temperature
